30ab95a51b2ce51d98ff8c15cfc38e17fb2284ec
manikanta <manikanta995975@gmail.com>
2026-02-28 08:19:56 +0530

    uploads

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config()
+dotenv.config({ path: path.join(__dirname, ".env") });
 const app = express()
 
 app.use(cors())
