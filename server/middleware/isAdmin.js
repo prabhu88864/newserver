0ad61d0c15a53ccc0fc58b468f4f40f1e66f30f1
manikanta <manikanta995975@gmail.com>
2026-03-08 12:54:19 +0530

    rolebased

--- server/middleware/isAdmin.js
+++ server/middleware/isAdmin.js
@@ -1,5 +1,5 @@
 export default function isAdmin(req, res, next) {
-  if (req.user?.role !== "ADMIN" && req.user?.role !== "MASTER") {
+  if (req.user?.role !== "ADMIN" && req.user?.role !== "MASTER" && req.user?.role !== "STAFF") {
     return res.status(403).json({ msg: "Admin only" });
   }
   next();
